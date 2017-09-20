eab0961186198e98a6dbee10cc6ec87cd2411cce
ZJC <492180653@qq.com>
2017-09-20 13:51:22 +0800

    5

--- src/main/java/com/smartlibrary/controller/UserController.java
+++ src/main/java/com/smartlibrary/controller/UserController.java
@@ -8,20 +8,26 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -142,66 +148,25 @@ public class UserController {
 	 */
 	@RequestMapping({"/editPhotourl"})
 	@ResponseBody
-	public Map<String,Object> editPhotourl(HttpServletRequest request, HttpServletResponse response)  throws ServletException, IOException {
+	public Map<String,Object> editPhotourl(HttpServletRequest request, HttpServletResponse response, @RequestParam MultipartFile file)  throws IOException {
 		
 		User user = new User();
 		user.setAccount(request.getParameter("account"));
 		user.setPhotourl(request.getParameter("photourl"));
-		
-		request.setCharacterEncoding("utf-8");  
-        response.setCharacterEncoding("utf-8");  
-        //1、创建一个DiskFileItemFactory工厂  
-        DiskFileItemFactory factory = new DiskFileItemFactory();  
-        //2、创建一个文件上传解析器  
-        ServletFileUpload upload = new ServletFileUpload(factory);  
-        //解决上传文件名的中文乱码  
-        upload.setHeaderEncoding("UTF-8");   
-        factory.setSizeThreshold(1024 * 500);//设置内存的临界值为500K  
-        File linshi = new File("E:\\linshi");//当超过500K的时候，存到一个临时文件夹中  
-        factory.setRepository(linshi);  
-        upload.setSizeMax(1024 * 1024 * 5);//设置上传的文件总的大小不能超过5M  
-        try {  
-            // 1. 得到 FileItem 的集合 items  
-            List<FileItem> /* FileItem */items = upload.parseRequest(request);  
-  
-            // 2. 遍历 items:  
-            for (FileItem item : items) {  
-                // 若是一个一般的表单域, 打印信息  
-                if (item.isFormField()) {  
-                    String name = item.getFieldName();  
-                    String value = item.getString("utf-8");  
-  
-                    System.out.println(name + ": " + value);  
-                      
-                      
-                }  
-                // 若是文件域则把文件保存到 e:\\files 目录下.  
-                else {  
-                    String fileName = item.getName();  
-                    long sizeInBytes = item.getSize();  
-                    System.out.println(fileName);  
-                    System.out.println(sizeInBytes);  
-  
-                    InputStream in = item.getInputStream();  
-                    byte[] buffer = new byte[1024];  
-                    int len = 0;  
-  
-                    fileName = "e:\\files\\" + fileName;//文件最终上传的位置  
-                    System.out.println(fileName);  
-                    OutputStream out = new FileOutputStream(fileName);  
-  
-                    while ((len = in.read(buffer)) != -1) {  
-                        out.write(buffer, 0, len);  
-                    }  
-  
-                    out.close();  
-                    in.close();  
-                }  
-            }  
-  
-        } catch (FileUploadException e) {  
-            e.printStackTrace();  
-        }  
+				 
+            if(file.isEmpty()){   
+                System.out.println("文件未上传");   
+            }else{   
+                System.out.println("文件长度: " + file.getSize());   
+                System.out.println("文件类型: " + file.getContentType());   
+                System.out.println("文件名称: " + file.getName());   
+                System.out.println("文件原名: " + file.getOriginalFilename());   
+                System.out.println("========================================");   
+                //如果用的是Tomcat服务器，则文件会上传到  {服务发布位置}\\WEB-INF\\upload\\文件夹中   
+                String realPath = request.getSession().getServletContext().getRealPath("/WEB-INF/upload");   
+                //这里不必处理IO流关闭的问题，因为FileUtils.copyInputStreamToFile()方法内部会自动把用到的IO流关掉，我是看它的源码才知道的   
+                FileUtils.copyInputStreamToFile(file.getInputStream(), new File(realPath, request.getParameter("account") + ".jpg"));   
+            }    
 		
 		return userService.editPhotourl(user);
 	}
